ecf5acaaa328030460b9691cb692dd9bfec098e6
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-06-18 17:59:57 -0300

    Refatora RepositorioRequisicaoEmArquivo para usar LINQ

--- ControleDeMedicamentosWeb.WebApp/Modulos/ModuloEstoque/Infra/RepositorioRequisicaoEmArquivo.cs
+++ ControleDeMedicamentosWeb.WebApp/Modulos/ModuloEstoque/Infra/RepositorioRequisicaoEmArquivo.cs
@@ -1,4 +1,3 @@
-
 using ControleDeMedicamentosWeb.WebApp.Compartilhado.Infra.Arquivos;
 using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloEstoque.Dominio;
 using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloEstoque.Dominio.Base;
@@ -7,45 +6,27 @@ namespace ControleDeMedicamentosWeb.WebApp.Modulos.ModuloEstoque.Infra;
 
 public class RepositorioRequisicaoEmArquivo : IRepositorioRequisicao
 {
-    protected readonly ContextoJson contexto;
-    protected readonly List<RequisicaoBase> registros;
+    private readonly ContextoJson contexto;
 
     public RepositorioRequisicaoEmArquivo(ContextoJson contexto)
     {
         this.contexto = contexto;
-        registros = contexto.Requisicoes;
     }
 
     public void Cadastrar(RequisicaoBase requisicao)
     {
-        registros.Add(requisicao);
+        contexto.Requisicoes.Add(requisicao);
 
         contexto.Salvar();
     }
 
     public List<RequisicaoEntrada> SelecionarRequisicoesEntrada()
     {
-        List<RequisicaoEntrada> requisicoesEntrada = new List<RequisicaoEntrada>();
-
-        foreach (RequisicaoBase req in registros)
-        {
-            if (req is RequisicaoEntrada reqEntrada)
-                requisicoesEntrada.Add(reqEntrada);
-        }
-
-        return requisicoesEntrada;
+        return contexto.Requisicoes.OfType<RequisicaoEntrada>().ToList();
     }
 
     public List<RequisicaoSaida> SelecionarRequisicoesSaida()
     {
-        List<RequisicaoSaida> requisicoesSaida = new List<RequisicaoSaida>();
-
-        foreach (RequisicaoBase req in registros)
-        {
-            if (req is RequisicaoSaida reqSaida)
-                requisicoesSaida.Add(reqSaida);
-        }
-
-        return requisicoesSaida;
+        return contexto.Requisicoes.OfType<RequisicaoSaida>().ToList();
     }
 }
